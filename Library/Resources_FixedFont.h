71620a145fb2d273d29d94bbb6964e59ec316799
Jonathan <jonathan.markland@btinternet.com>
2017-04-26 23:27:13 +0100

    Fonts added to library.

---
+++ Library/Resources_FixedFont.h
@@ -0,0 +1,6 @@
+
+#pragma once
+
+extern uint8_t  g_PcBios8x16Font[256*16];    // This font is 8*16 (full cell used). Start=0, lines=16
+extern uint8_t  g_CamputersLynxIIFont[970];  // This font's character pixels reside in bits 6..1 inclusive. Start=32, lines=10
+
