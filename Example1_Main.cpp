b23a3573a49df488f9eba41fcb091b90aa53aba9
Jonathan <jonathan.markland@btinternet.com>
2017-04-26 21:32:18 +0100

    Demo program

--- Example1_Main.cpp
+++ Example1_Main.cpp
@@ -1,10 +1,95 @@
 
+#include <fstream>
+#include <string>
 #include <iostream>
-#include "SineTable.h"
+#include "libGraphics_Main.h"
+#include "ColoursEnum.h"
+
+
+
+
+const int DemoBitmapWidth = 640;
+const int DemoBitmapHeight = 480;
+
+
+
+
+
+bool SaveMemoryToFile(const std::string &filename, const void *data, size_t const bytes)
+{
+	std::ofstream b_stream( filename.c_str(), std::fstream::out | std::fstream::binary );
+	if (b_stream)
+	{
+		b_stream.write((const char *) data, bytes);
+		return b_stream.good();
+	}
+	return false;
+}
+
+
 
  
 int main()
 {
-  std::cout << "Hello World!" << g_FixedPointSineTable[45] << std::endl;
-  return 0;
+	//std::cout << "Hello World!" << g_FixedPointSineTable[45] << std::endl;
+
+	//
+	// In this version of the library, the 32-bpp bitmap class "libGraphics::Bitmaps::Colour"
+	// doesn't allocate bitmap memory.  So we do it separately:
+	//
+	
+	auto sizeBytes = DemoBitmapWidth * DemoBitmapHeight * 4;
+	void *demoBitmapMemory = malloc( sizeBytes );
+	if( demoBitmapMemory == nullptr ) return 1;
+
+	// Just ensure memory is clean.
+	memset( demoBitmapMemory, 0, sizeBytes );
+	
+	// Now set up a bitmap object to reference this memory:
+	libGraphics::Bitmaps::Colour  theColourBitmap(
+		(uint32_t *) demoBitmapMemory,
+		DemoBitmapWidth, DemoBitmapHeight,
+		DemoBitmapWidth * 4 ); // <-- inter-row offset, in bytes
+
+	// Create a "device" object that will allow drawing routines to operate
+	// without knowing what they're drawing on:
+	libGraphics::Devices::BitmapDevice  theBitmapDevice( theColourBitmap );
+
+	// Plug in the "Font server":
+	// theBitmapDevice.SetFontServer( testFontServer );
+
+	//
+	// Graphics start here...
+	//
+	
+	// Note the colour values are 32-bpp, so you should specify RGBA where A=255
+	// which will let you see the output in GIMP.  Obviously the "A" component is
+	// not processed by this library, only stored.  Alpha just isn't retro enough.
+	
+	// Create pens:
+	auto whitePen = std::make_shared<libGraphics::Pens::Solid>( libBasic::Colours::White );
+	auto bluePen  = std::make_shared<libGraphics::Pens::Solid>( libBasic::Colours::Blue );
+
+	// Create brushes:
+	auto patternedBrush = std::make_shared<libGraphics::Brushes::Patterned>( 
+		g_Pattern1616_Balls, libBasic::Colours::Yellow, libBasic::Colours::Green );
+
+	auto solidBrush = std::make_shared<libGraphics::Brushes::Solid>( 
+		libBasic::Colours::Blue );
+
+	// Demo the "direct" functions, although these are for bitmap devices only:
+
+	theBitmapDevice.SelectBrush( solidBrush );
+	theBitmapDevice.DirectRectangle( 0,0, 400,400 );
+	
+	theBitmapDevice.SelectBrush( patternedBrush );
+	theBitmapDevice.DirectRectangle( 50,50, 100,100 );
+	theBitmapDevice.DirectEllipse( 200,50, 250,100 );
+	
+//	theBitmapDevice.DirectTriangle( 400,50, 450,100, 350,100 );
+
+	// Save the file as ".data" so you can import it into GIMP and use the RAW import
+	// format.  You would enter 640*480 as the dimensions, and set the format to RGBA:
+
+	return SaveMemoryToFile( "output.data", demoBitmapMemory, sizeBytes ) ? 0 : 1;
 }
