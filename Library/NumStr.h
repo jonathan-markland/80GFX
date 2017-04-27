4c9630c5fbade41b1354f46db09c4d4c06f5c835
Jonathan <jonathan.markland@btinternet.com>
2017-04-27 23:43:59 +0100

    GPL notices

--- Library/NumStr.h
+++ Library/NumStr.h
@@ -1,3 +1,23 @@
+//
+// 80GFX - Totally retro 1980s graphics library for C++ with no dependencies.
+// Copyright (C) 2017  Jonathan Markland
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//		jynx_emulator {at} yahoo {dot} com
+//
+
 
 // Num, Uns, Hex, Bin
 // NumStr, UnsStr
