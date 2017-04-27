5b81aa87b632f667bbe396030fc79071e91af38d
Jonathan <jonathan.markland@btinternet.com>
2017-04-27 21:10:19 +0100

    Upgrade Example1 to use the new common code.

--- Example1_Main.cpp
+++ Example1_Main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "libGraphics_Main.h"
 #include "ColoursEnum.h"
+#include "AssistanceForExamples.h"
 
 
 
@@ -13,72 +14,39 @@ const int DemoBitmapHeight = 480;
 
 
 
-
-
-bool SaveMemoryToFile(const std::string &filename, const void *data, size_t const bytes)
+		
+void DrawWithTheDirectGraphicsFunctions( libGraphics::Devices::BitmapDevice &theBitmapDevice )
 {
-	std::ofstream b_stream( filename.c_str(), std::fstream::out | std::fstream::binary );
-	if (b_stream)
-	{
-		b_stream.write((const char *) data, bytes);
-		return b_stream.good();
-	}
-	return false;
-}
-
-
-
- 
-int main()
-{
-	//
-	// In this version of the library, the 32-bpp bitmap class "libGraphics::Bitmaps::Colour"
-	// doesn't allocate bitmap memory.  So we do it separately:
-	//
-	
-	auto sizeBytes = DemoBitmapWidth * DemoBitmapHeight * 4;
-	void *demoBitmapMemory = malloc( sizeBytes );
-	if( demoBitmapMemory == nullptr ) return 1;
-
-	// Just ensure memory is clean.
-	memset( demoBitmapMemory, 0, sizeBytes );
-	
-	// Now set up a bitmap object to reference this memory:
-	libGraphics::Bitmaps::Colour  theColourBitmap(
-		(uint32_t *) demoBitmapMemory,
-		DemoBitmapWidth, DemoBitmapHeight,
-		DemoBitmapWidth * 4 ); // <-- inter-row offset, in bytes
-
-	// Create a "device" object that will allow drawing routines to operate
-	// without knowing what they're drawing on:
-	libGraphics::Devices::BitmapDevice  theBitmapDevice( theColourBitmap );
-
-	//
-	// Graphics start here...
-	//
-	
 	// Note the colour values are 32-bpp, so you should specify RGBA where A=255
 	// which will let you see the output in GIMP.  Obviously the "A" component is
 	// not processed by this library, only stored.  Alpha just isn't retro enough.
 	
 	// Create pens:
+	
 	auto whitePen = std::make_shared<libGraphics::Pens::Solid>( libBasic::Colours::White );
 	auto bluePen  = std::make_shared<libGraphics::Pens::Solid>( libBasic::Colours::Blue );
 
+	
+	
 	// Create brushes:
+	
 	auto patternedBrush = std::make_shared<libGraphics::Brushes::Patterned>( 
 		g_Pattern1616_Balls, libBasic::Colours::Yellow, libBasic::Colours::Green );
 
 	auto solidBrush = std::make_shared<libGraphics::Brushes::Solid>( 
 		libBasic::Colours::Blue );
 
-	// Demo the "direct" functions, although these are for bitmap devices only:
+		
+		
+	// Demo the "direct" functions, although these are for BitmapDevice only:
 
 	theBitmapDevice.SelectBrush( solidBrush );
 	theBitmapDevice.DirectRectangle( 0,0, 400,400 );
 	
 	theBitmapDevice.SelectBrush( patternedBrush );
 	theBitmapDevice.DirectRectangle( 50,50, 100,100 );
+
+	
 
 	// In order to see "direct" ellipse and triangle, we must do a once-only creation
 	// of a temporary array to store the edges of each raster line of these shapes.
@@ -104,9 +72,17 @@ int main()
 	
 	theBitmapDevice.SetLRArray(nullptr, 0);
 	delete [] lrArray;
+}
 
-	// Save the file as ".data" so you can import it into GIMP and use the RAW import
-	// format.  You would enter 640*480 as the dimensions, and set the format to RGBA:
 
-	return SaveMemoryToFile( "output.data", demoBitmapMemory, sizeBytes ) ? 0 : 1;
+	
+
+
+int main()
+{
+	return WithNewBitmapDo( DemoBitmapWidth, DemoBitmapHeight, "output.data", 
+		[]( libGraphics::Devices::BitmapDevice &theDevice )
+		{
+			DrawWithTheDirectGraphicsFunctions( theDevice );
+		}) ? 0 : 1;
 }
